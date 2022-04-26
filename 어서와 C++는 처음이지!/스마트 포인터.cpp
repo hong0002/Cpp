f8f3626f053e32b8ab8b474b6dbadfd51fb79467
hong0002 <34588777+hong0002@users.noreply.github.com>
2022-04-26 10:52:01 +0900

    스마트 포인터

---
+++ 어서와 C++는 처음이지!/스마트 포인터.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+int main()
+{
+	while (1)
+	{
+		unique_ptr<int[]>buf(new int[10]);
+
+		for (int i = 0; i < 10; i++)
+		{
+			buf[i] = i;
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			cout << buf[i] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
